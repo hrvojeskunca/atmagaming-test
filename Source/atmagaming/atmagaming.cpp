4630c4e126bdd6bd11973818013561a784af12f0
Solferin <hrvojeskunca@users.noreply.github.com>
2024-10-24 10:44:40 +0200

    Init commit

---
+++ Source/atmagaming/atmagaming.cpp
@@ -0,0 +1,6 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "atmagaming.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, atmagaming, "atmagaming" );
